b07b5f3058814c857544bc2737a5e0ad5a47a328
Jacob Stevens <jstevens1263@gmail.com>
2022-04-10 17:31:11 -0700

    Added module_5 folder and mongodb_test.py

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,26 @@
+#Imports
+from pymongo import MongoClient
+
+#
+#   Title: mongodb_test.py
+#   Author: Jacob Stevens
+#   Date: 10 April 2022
+#   Description: Test program for connecting to a
+#                 MongoDB Atlas cluster
+
+
+# MongoDB connection string
+URI = "mongodb+srv://admin:admin@mycluster.w4rfd.mongodb.net/pytech?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
+
+# connect to the MongoDB cluster
+client = MongoClient(URI)
+
+# connect pytech database
+db = client.pytech
+
+# show the connected collections
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")
