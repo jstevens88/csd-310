3b1c8e7156ca2fe7e191001d9e8c81549c1a02e0
Jacob Stevens <jstevens1263@gmail.com>
2022-04-17 07:10:19 -0700

    Updated pytech_delete.py

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -30,38 +30,39 @@ print("\n**** DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY ****")
 for item in student_list:
     print("\tStudent ID: " + item["student_id"] + "\n\tFirst Name: " + item["first_name"] + "\n\tLast Name: " + item["last_name"] + "\n")
 
-
+# create new entry to insert into database
 new_doc = {
     "student_id": "1010",
     "first_name": "Jenessica",
     "last_name": "Myers"
 }
-
+# insert new_doc and store the _id in variable
 new_doc_id = students.insert_one(new_doc).inserted_id
 
-# insert statements with output 
+# insert statements with output
 print("\n****INSERT STATEMENTS****")
 print("Inserted student record into the students collection with document_id " + str(new_doc_id))
 
 # call the find_one() method by student_id 1010
 student_find_doc = students.find_one({"student_id": "1010"})
 
-# display the results 
+# display results
 print("\n****DISPLAYING NEW STUDENT DOC****")
 print("\tStudent ID: " + student_find_doc["student_id"] + "\n\tFirst Name: " + student_find_doc["first_name"] + "\n\tLast Name: " + student_find_doc["last_name"] + "\n")
 
-# call the delete_one method to remove the student_find_doc
+# call the delete_one() method to remove the student_find_doc
 deleted_student_find_doc = students.delete_one({"student_id": "1010"})
 
-# find all students in the collection 
+
+# find all students in the collection
 new_student_list = students.find({})
 
-# display message 
+# display message
 print("\n****DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY AFTER delete_one()****")
 
-# loop over the collection and output the results 
+# loop over the new collection and output the results
 for doc in new_student_list:
     print("\tStudent ID: " + doc["student_id"] + "\n\tFirst Name: " + doc["first_name"] + "\n\tLast Name: " + doc["last_name"] + "\n")
 
-# exit message 
+# exit message
 input("\n\n  End of program, press any key to continue...")
