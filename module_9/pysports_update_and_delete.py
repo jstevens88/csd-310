68cccba2fa9ae0455f4651ecba5b5e6d4db19ff1
Jacob Stevens <jstevens1263@gmail.com>
2022-04-30 06:51:40 -0700

    fixed multiple Smeagol entries

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -16,25 +16,6 @@ config = {
     "raise_on_warnings": True
 }
 
-
-
-def show_players(cursor, title):
-    """ method to execute an inner join on the player and team table, 
-        iterate over the dataset and output the results to the terminal window.
-    """
-
-    # inner join query 
-    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
-
-    # get the results from the cursor object 
-    players = cursor.fetchall()
-
-    print("\n  -- {} --".format(title))
-    
-    # iterate over the player data set and display the results 
-    for player in players:
-        print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
-
 #Catch any potential errors
 try:
     #connect to the pysports database 
@@ -43,20 +24,25 @@ try:
     
     
     # insert player query 
-    add_player = ("INSERT INTO player(first_name, last_name, team_id)"
-                 "VALUES(%s, %s, %s)")
+    cursor.execute("INSERT INTO player(first_name, last_name, team_id) VALUES('Smeagol','Shire Folk', 1);")
+    
 
-    # player data fields 
-    player_data = ("Smeagol", "Shire Folk", 1)
-
-    # insert a new player record
-    cursor.execute(add_player, player_data)
+    
 
     # commit the insert to the database 
     db.commit()
 
-    # show all records in the player table 
-    show_players(cursor, "DISPLAYING PLAYERS AFTER INSERT")
+    # inner join query 
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
+
+    # get the results from the cursor object 
+    players = cursor.fetchall()
+
+    print("\n  -- DISPLAYING PLAYERS AFTER INSERT --")
+    
+    # iterate over the player data set and display the results 
+    for player in players:
+        print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
 
     # update the newly inserted record 
     update_player = ("UPDATE player SET team_id = 2, first_name = 'Gollum', last_name = 'Ring Stealer' WHERE first_name = 'Smeagol'")
@@ -64,16 +50,34 @@ try:
     # execute the update query
     cursor.execute(update_player)
 
-    # show all records in the player table 
-    show_players(cursor, "DISPLAYING PLAYERS AFTER UPDATE")
+    # inner join query 
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
+
+    # get the results from the cursor object 
+    players = cursor.fetchall()
+
+    print("\n  -- DISPLAYING PLAYERS AFTER UPDATE --")
+    
+    # iterate over the player data set and display the results 
+    for player in players:
+        print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
 
     # delete query 
     delete_player = ("DELETE FROM player WHERE first_name = 'Gollum'")
 
     cursor.execute(delete_player)
+    
+    # inner join query 
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
 
-    # show all records in the player table 
-    show_players(cursor, "DISPLAYING PLAYERS AFTER DELETE")
+    # get the results from the cursor object 
+    players = cursor.fetchall()
+
+    print("\n  -- DISPLAYING PLAYERS AFTER DELETE --")
+    
+    # iterate over the player data set and display the results 
+    for player in players:
+        print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
 
     input("\n\n  Press any key to continue... ")
     
